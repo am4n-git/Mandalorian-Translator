907d071ca0c44802138eabd0236b481b97ba2cb0
am4n-raj <am4nraj@gmail.com>
2021-09-17 16:27:28 +0530

    complete

---
+++ app.js
@@ -0,0 +1,59 @@
+var btn= document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#text-input");
+var output = document.querySelector("#output");
+var samplebtn = document.querySelector("#sampletext");
+var clearbtn= document.querySelector("#clear");
+
+
+var url = "https://api.funtranslations.com/translate/mandalorian.json";
+
+function getTranslation(text){
+    return url + "?" + "text="+text;
+}
+
+function errorHandler(error){
+    alert("Too Many Requests: Rate limit of 5 requests per hour exceeded.\n Try again later!!");
+}
+
+function clickHandler(){
+
+    var inputText = txtInput.value
+
+    if(inputText==""){
+        alert("Enter your message in the box");
+    }
+
+    else{
+        fetch(getTranslation(inputText))
+            .then(response => response.json())
+            .then(json => {
+            var translatedText= json.contents.translated;
+            output.innerText= translatedText;
+        })
+            .catch(errorHandler)
+    }
+
+};
+
+
+function sampleEvent()
+{
+    txtInput.value="Debugging is like being the detective in a crime drama where you are also the murderer.";
+
+};
+
+function clear(){
+    txtInput.value= "";
+    output.innerText= "";
+};
+
+
+
+
+btn.addEventListener("click", clickHandler);
+samplebtn.addEventListener("click", sampleEvent);
+clearbtn.addEventListener("click", clear);
+
+
+
+
